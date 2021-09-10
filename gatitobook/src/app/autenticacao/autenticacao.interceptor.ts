6551c0011c3ada93de12169e0325bd127cc7b917
Matheus Rufino Cruz <mrcruz1@stefanini.com>
2021-09-10 07:41:45 -0300

    ADD: active router e interceptor antes da refatoração

--- gatitobook/src/app/autenticacao/autenticacao.interceptor.ts
+++ gatitobook/src/app/autenticacao/autenticacao.interceptor.ts
@@ -6,13 +6,20 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TokenService } from './token.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class AutenticacaoInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private tokenService: TokenService, ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if(this.tokenService.possuiToken()){
+        const token = this.tokenService.retornaToken();
+        const headers = new HttpHeaders().append('x-access-token', token);
+        request = request.clone({headers})
+    }
     return next.handle(request);
   }
 }
