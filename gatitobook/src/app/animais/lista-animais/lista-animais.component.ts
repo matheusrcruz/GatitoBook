6ca80b71b1c1f1f6ba794e64538e198351102ba4
Matheus Rufino Cruz <mrcruz1@stefanini.com>
2021-09-20 19:44:01 -0300

    FIX: rota de animais por passagem de parametro

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { Animais } from '../animais';
 import { AnimaisService } from '../animais.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lista-animais',
@@ -12,15 +12,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
-  animais$!: Animais;
+  animais!: Animais;
 
-  constructor(
-    private activatedRoute: ActivatedRoute
-  ) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(param => {
-      this.animais$ = this.activatedRoute.snapshot.data['animais'];
-    })
+    this.activatedRoute.params.subscribe((param) => {
+      this.animais = this.activatedRoute.snapshot.data['animais'];
+    });
   }
 }
